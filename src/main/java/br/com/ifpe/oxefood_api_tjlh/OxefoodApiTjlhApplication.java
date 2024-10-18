092c8d602c8b380258de5b031650edb03c725ca4
thiagojorgelins <tjlh@discente.ifpe.edu.br>
2024-10-18 19:31:05 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/ifpe/oxefood_api_tjlh/OxefoodApiTjlhApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_tjlh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiTjlhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiTjlhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood_api_tjlh/OxefoodApiTjlhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_tjlh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiTjlhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
