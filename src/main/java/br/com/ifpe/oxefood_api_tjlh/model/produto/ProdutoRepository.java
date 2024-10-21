866f928e3fe52a98552dad074976f14eaf26a47d
thiagojorgelins <tjlh@discente.ifpe.edu.br>
2024-10-21 11:00:34 -0300

    Add entidade produto

---
+++ src/main/java/br/com/ifpe/oxefood_api_tjlh/model/produto/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.oxefood_api_tjlh.model.produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+  
+}
