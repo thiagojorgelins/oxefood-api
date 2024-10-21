343b41fb9b9ad90a458b99cc10b18fd3046bafc9
thiagojorgelins <thiago.jorge2001@gmail.com>
2024-10-20 23:32:59 -0300

    Add model categoria produto

---
+++ src/main/java/br/com/ifpe/oxefood_api_tjlh/model/categoria/CategoriaProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.oxefood_api_tjlh.model.categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long>{
+
+}
