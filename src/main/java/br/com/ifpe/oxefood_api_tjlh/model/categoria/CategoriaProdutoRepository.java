6a336d0978494d16ef78193087869748740172ed
thiagojorgelins <tjlh@discente.ifpe.edu.br>
2024-10-21 08:46:43 -0300

    Rm model Categoria Produto

--- src/main/java/br/com/ifpe/oxefood_api_tjlh/model/categoria/CategoriaProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ifpe.oxefood_api_tjlh.model.categoria;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long>{
-
-}
