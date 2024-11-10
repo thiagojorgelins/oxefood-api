47497791834f036b5a8a6df3d397bbb79c765d09
thiagojorgelins <thiago.jorge2001@gmail.com>
2024-11-10 20:09:44 -0300

    feat: add listagem de entregador

--- src/main/java/br/com/ifpe/oxefood_api_tjlh/model/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood_api_tjlh/model/entregador/EntregadorService.java
@@ -1,24 +1,34 @@
 package br.com.ifpe.oxefood_api_tjlh.model.entregador;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood_api_tjlh.model.cliente.Cliente;
 import jakarta.transaction.Transactional;
 
 @Service
 public class EntregadorService {
-  
+
   @Autowired
   private EntregadorRepository repository;
 
   @Transactional
-  public Entregador save(Entregador entregador){
+  public Entregador save(Entregador entregador) {
 
     entregador.setHabilitado(Boolean.TRUE);
     entregador.setVersao(1L);
     entregador.setDataCriacao(LocalDate.now());
     return repository.save(entregador);
+  }
+
+  public List<Entregador> listarTodos() {
+    return repository.findAll();
+  }
+
+  public Entregador obterPorID(Long id) {
+    return repository.findById(id).get();
   }
 }
